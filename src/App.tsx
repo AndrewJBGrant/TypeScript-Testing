0976c1b9bc67a5d4286ddd7506d2f47c1a322dab
Andrew Grant <andrewgrant2009@hotmail.com>
2023-07-21 17:09:28 +0200

    added more styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import vitestLogo from './assets/vitest.svg'
+import typeScriptLogo from './assets/typescript.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(2)
 
   return (
     <>
@@ -15,10 +17,16 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vitest.dev/guide/" target="_blank">
+          <img src={vitestLogo} className="logo vitest" alt="Vitest logo" />
+        </a>
+        <a href="https://www.typescriptlang.org/docs/" target="_blank">
+          <img src={typeScriptLogo} className="logo typescript" alt="TypeScript logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count * 2)}>
           count is {count}
         </button>
         <p>
